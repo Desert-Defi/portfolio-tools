a6f4573a51ae452fd98d863f85cd28ada02fee0b
jgrizzled <justin.greene5@gmail.com>
2020-04-04 12:15:13 -0700

    fix backtesting on returns

--- src/backtester.js
+++ src/backtester.js
@@ -4,6 +4,7 @@ Params
   checkRebalance: function(currentWeights, newWeights, dateIndex, lastRebalanceIndex, options, context) returns bool
   pricesByAsset: 2D array of prices by asset [ asset1Prices[], asset2Prices[], ... ]
   options (optional): object that gets passed to calcWeights and checkRebalance with strategy options
+    options.isReturns: set to true if passing in returns series for pricesByAsset[]
   context (optional): object that gets passed to calcWeights and checkRebalance with contextual data
 
 Returns
@@ -29,9 +30,36 @@ export default function backtester(
   const weightsByAsset = Array(pricesByAsset.length)
     .fill(0)
     .map((w) => [0]);
-
   let currentWeights = Array(pricesByAsset.length).fill(0);
   let lastRebalanceIndex = null;
+  let newWeights = null;
+
+  // set weights for first day
+  if (options.isReturns) {
+    newWeights = calcWeights(0, pricesByAsset, options, context);
+    // check if should rebalance
+    if (
+      checkRebalance(
+        currentWeights,
+        newWeights,
+        dateIndex,
+        lastRebalanceIndex,
+        options,
+        context
+      )
+    ) {
+      // check if new weight calculations differ from current
+      if (currentWeights.some((cw, i) => cw !== newWeights[i])) {
+        currentWeights = newWeights;
+        lastRebalanceIndex = 0;
+        // record weights
+        weightsByAsset.forEach(
+          (assetWeights, assetIndex) =>
+            (assetWeights[0] = currentWeights[assetIndex])
+        );
+      }
+    }
+  }
   // iterate through dates
   for (
     let dateIndex = options.isReturns ? 0 : 1;
@@ -50,7 +78,7 @@ export default function backtester(
     }, 0);
 
     // calc new weights
-    const newWeights = calcWeights(dateIndex, pricesByAsset, options, context);
+    newWeights = calcWeights(dateIndex, pricesByAsset, options, context);
 
     // check if should rebalance
     if (
