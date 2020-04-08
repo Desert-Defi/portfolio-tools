ba5ed0b23bc60cca89cb4b3a9575c7a28aa9443a
Alex Fryer <alex.jeffrey.fryer@gmail.com>
2020-04-08 13:05:08 -0700

    fixed imports on hurst

--- src/math/hurst.js
+++ src/math/hurst.js
@@ -1,6 +1,10 @@
 const { add } = require('mathjs');
 const { linearRegression } = require('simple-statistics');
 
+import { add } from 'mathjs';
+import { linearRegression } from 'simple-statistics';
+import PortfolioAnalytics from 'portfolio-analytics';
+
 export const hurstExponent = (series) => {
   let s = series;
   let y = [];
@@ -9,7 +13,7 @@ export const hurstExponent = (series) => {
     .fill(0)
     .map((e, i) => i);
   for (const i in k) {
-    y.push(sampleStdDev(u));
+    y.push(PortfolioAnalytics.sampleStddev_(u));
     u = 0.5 * (u.filter((v, i) => i % 2 == 0) + u.filter((v, i) => i % 2 != 0));
   }
   let xy = [add(k, 1), y.map((e) => Math.log2(e))].reduce((a, b) =>
